535f1b780a6a8a9fc32f92f66fcf62297b124b34
AsphodelRem <asphodel.bog.rem@gmail.com>
2026-02-15 22:44:58 +0700

    Update base interfaces for storages

--- ragu/storage/base_storage.py
+++ ragu/storage/base_storage.py
@@ -1,10 +1,12 @@
-from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Union, Generic, TypeVar, List, Set
+from abc import ABC, abstractmethod
+from typing import Union, Generic, TypeVar, List, Set, Optional, Tuple
 
 import numpy as np
 
 from ragu.graph.types import Entity, Relation
+
+EdgeSpec = Tuple[str, str, Optional[str]]
 
 
 @dataclass
@@ -32,6 +34,10 @@ class BaseVectorStorage(BaseStorage, ABC):
     async def upsert(self, data: dict[str, np.ndarray]):
         ...
 
+    @abstractmethod
+    async def delete(self, ids: List[str]) -> None:
+        ...
+
 
 T = TypeVar("T")
 
@@ -49,15 +55,16 @@ class BaseKVStorage(Generic[T], BaseStorage, ABC):
     async def get_by_ids(self, ids: list[str], fields: Union[set[str], None] = None) -> List[Union[T, None]]:
         ...
 
-    async def index_done_callback(self):
-        pass
-
     @abstractmethod
     async def filter_keys(self, data: List[str]) -> Set[str]:
         ...
 
     @abstractmethod
     async def upsert(self, data: dict[str, T]):
+        ...
+
+    @abstractmethod
+    async def delete(self, ids: List[str]) -> None:
         ...
 
     @abstractmethod
@@ -68,47 +75,7 @@ class BaseKVStorage(Generic[T], BaseStorage, ABC):
 @dataclass
 class BaseGraphStorage(BaseStorage, ABC):
     @abstractmethod
-    async def has_node(self, node_id: str) -> bool:
-        ...
-
-    @abstractmethod
-    async def has_edge(self, source_node_id: str, target_node_id: str) -> bool:
-        ...
-
-    @abstractmethod
-    async def node_degree(self, node_id: str) -> int:
-        ...
-
-    @abstractmethod
-    async def get_node(self, node_id: str) -> Entity | None:
-        ...
-
-    @abstractmethod
-    async def get_edge(self, source_node_id: str, target_node_id: str) -> Relation | None:
-        ...
-
-    @abstractmethod
-    async def get_node_edges(self, source_node_id: str) -> List[Relation]:
-        ...
-
-    @abstractmethod
-    async def upsert_node(self, node: Entity):
-        ...
-
-    @abstractmethod
-    async def upsert_edge(self, edge: Relation):
-        ...
-
-    @abstractmethod
-    async def take_only_largest_component(self):
-        ...
-
-    @abstractmethod
-    async def remove_isolated_nodes(self):
-        ...
-
-    @abstractmethod
-    async def cluster(self, max_cluster_size: int=128, **additional_cluster_kwargs):
+    async def edges_degrees(self, edge_specs: List[EdgeSpec]) -> List[int]:
         ...
 
     @abstractmethod
@@ -117,4 +84,40 @@ class BaseGraphStorage(BaseStorage, ABC):
 
     @abstractmethod
     async def index_done_callback(self):
+        ...
+
+    @abstractmethod
+    async def get_nodes(self, node_ids: List[str]) -> List[Optional[Entity]]:
+        ...
+
+    @abstractmethod
+    async def upsert_nodes(self, nodes: List[Entity]) -> None:
+        ...
+
+    @abstractmethod
+    async def delete_nodes(self, node_ids: List[str]) -> None:
+        ...
+
+    @abstractmethod
+    async def get_edges(self, edge_specs: List[EdgeSpec]) -> List[Optional[Relation]]:
+        ...
+
+    @abstractmethod
+    async def upsert_edges(self, edges: List[Relation]) -> None:
+        ...
+
+    @abstractmethod
+    async def delete_edges(self, edge_specs: List[EdgeSpec]) -> None:
+        ...
+
+    @abstractmethod
+    async def get_all_edges_for_nodes(self, node_ids: List[str]) -> List[List[Relation]]:
+        ...
+
+    @abstractmethod
+    async def get_all_nodes(self) -> List[Entity]:
+        ...
+
+    @abstractmethod
+    async def get_all_edges(self) -> List[Relation]:
         ...
